8cdee41a231de1144b1af78baf650ef8d3eb3e55
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-02-21 18:58:24 +0530

    modified cors

--- api/index.js
+++ api/index.js
@@ -22,6 +22,12 @@ const secretKey = 'aakashrajbhar25'
 // app.use(cors({credentials:true,origin:'https://aakash-blog-website.vercel.app/'}))
 app.use(cors())
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
