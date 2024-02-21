7d8080ec55bc060d5aad135a58a845ec2de2b0a2
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-02-21 16:42:12 +0530

    added required dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+// import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
