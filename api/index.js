6b210626615a700609e8585a52c8c414b6ea774b
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-02-21 20:50:57 +0530

    modified cors

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 const salt = bcrypt.genSaltSync(10);
 const secretKey = 'aakashrajbhar25'
 
-app.use(cors())
+// app.use(cors())
 // app.use(cors({credentials:true,origin:'https://aakash-blog-website.netlify.app/'}))
 
 
